80978a4f0826e9a327a7c4a9bea989838b6b4073
Diego Sullcaray Ramos <u20211c348@upc.edu.pe>
2025-08-11 02:47:21 -0500

    dd

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 import { LayoutRoutingModule } from './layout-routing.module';
-
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    BreadcrumbComponent
+  ],
   imports: [
     CommonModule,
+    RouterModule,
+    PrimeNgModule,
     LayoutRoutingModule
   ]
 })
